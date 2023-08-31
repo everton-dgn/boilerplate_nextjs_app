d22d41619521791d54c6c2101a94527bf717bd07
Éverton Toffanetto <evertondgn@hotmail.com>
2023-08-31 19:03:49 -0300

    test: ensures counter hook works

---
+++ src/hooks/useCount/__tests__/useCount.test.ts
@@ -0,0 +1,17 @@
+import { act } from '@testing-library/react'
+import renderHooksProvider from 'testHelpers/providers/hooks'
+import useCount from '..'
+
+describe('[Hook] useCount', () => {
+  it('should increment wnhen call count', async () => {
+    const { result } = renderHooksProvider(() => useCount())
+
+    expect(result.current.count).toBe(0)
+
+    act(() => result.current.increment())
+    expect(result.current.count).toBe(1)
+
+    act(() => result.current.increment())
+    expect(result.current.count).toBe(2)
+  })
+})
