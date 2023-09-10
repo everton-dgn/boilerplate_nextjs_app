1ed9b1f9f5d7419ba8d2c18e936d9d612577cc0c
Éverton Toffanetto <evertondgn@hotmail.com>
2023-09-10 00:25:30 -0300

    doc: improvements storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,14 @@
 import type { Preview } from '@storybook/react'
 import viewports from './viewports'
 import '../src/theme/reset.scss'
+import './style.scss'
 
 const preview: Preview = {
   parameters: {
     viewport: {
       viewports
     },
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: {argTypesRegex: '^on[A-Z].*'},
     controls: {
       matchers: {
         color: /(background|color)$/i,
