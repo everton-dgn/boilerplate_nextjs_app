9bde08b1819ff22896bbae28ba0128aa4ba0d5d2
Éverton Toffanetto <evertondgn@hotmail.com>
2023-09-20 22:12:49 -0300

    chore: improvements stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,6 +11,7 @@ module.exports = {
     'stylelint-high-performance-animation'
   ],
   rules: {
+    'declaration-block-no-duplicate-properties': true,
     'max-nesting-depth': 2,
     'no-empty-source': null,
     'custom-property-pattern': '^([a-z][a-z0-9]*)((-|--)[a-z0-9]+)*$',
