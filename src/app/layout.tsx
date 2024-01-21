85bed2cb58881ded87b7d482db7c8ba1c772be40
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-21 06:05:26 -0300

    fix: apply font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,28 +5,26 @@ import poppins from 'theme/fontFamily'
 import { MainProvider } from 'providers/mainProvider'
 import 'theme/reset.scss'
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <html lang="pt-BR">
-      <head>
-        <meta
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-          name="viewport"
-        />
-        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-        <meta content="Project frontend with nextjs" name="description" />
-        <meta content="boilerplate, nextjs, reactjs, react" name="keywords" />
-        <meta content="© Éverton Toffanetto" name="copyright" />
-        <meta content="#fff" name="theme-color" />
-        <link href="/favicon.png" rel="icon" type="image/png" />
-        <link href="/favicon.png" rel="apple-touch-icon" />
-      </head>
+const RootLayout = ({ children }: { children: ReactNode }) => (
+  <html lang="pt-BR">
+    <head>
+      <meta
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+        name="viewport"
+      />
+      <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+      <meta content="Project frontend with nextjs" name="description" />
+      <meta content="boilerplate, nextjs, reactjs, react" name="keywords" />
+      <meta content="© Éverton Toffanetto" name="copyright" />
+      <meta content="#fff" name="theme-color" />
+      <link href="/favicon.png" rel="icon" type="image/png" />
+      <link href="/favicon.png" rel="apple-touch-icon" />
+    </head>
 
-      <body className={poppins.className}>
-        <MainProvider>{children}</MainProvider>
-      </body>
-    </html>
-  )
-}
+    <body className={poppins.className}>
+      <MainProvider>{children}</MainProvider>
+    </body>
+  </html>
+)
 
 export default RootLayout
