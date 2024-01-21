e944a65bdbc82769403de554db8bce83306cbc11
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-21 19:28:43 -0300

    refactor: simplify code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,35 +2,12 @@
 
 import { useRouter } from 'next/navigation'
 
-import poppins from 'theme/fontFamily'
-
 import { Error } from 'components/organisms'
 
 const NotFound = () => {
   const { replace } = useRouter()
 
-  return (
-    <html lang="pt-BR">
-      <head>
-        <meta
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-          name="viewport"
-        />
-        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-        <meta content="Project frontend with nextjs" name="description" />
-        <meta content="boilerplate, nextjs, reactjs, react" name="keywords" />
-        <meta content="© Éverton Toffanetto" name="copyright" />
-        <meta content="#fff" name="theme-color" />
-        <link href="/favicon.png" rel="icon" type="image/png" />
-        <link href="/favicon.png" rel="apple-touch-icon" />
-        <title>Error Page</title>
-      </head>
-
-      <body className={poppins.className}>
-        <Error reset={() => replace('/')} />
-      </body>
-    </html>
-  )
+  return <Error reset={() => replace('/')} />
 }
 
 export default NotFound
