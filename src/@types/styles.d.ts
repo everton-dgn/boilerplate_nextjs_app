9ba1c8c2182734cbf6c8a8c34cfba0481320b7d0
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-25 12:13:27 -0300

    chore: update types

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,2 +1,11 @@
-declare module '*.module.css'
-declare module '*.module.scss'
+declare module '*.css' {
+  // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+  const content: { [className: string]: string }
+  export = content
+}
+
+declare module '*.scss' {
+  // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
+  const content: { [className: string]: string }
+  export = content
+}
