fad60981ec887858db383467844aeb0487c66d3e
Éverton Toffanetto <evertondgn@hotmail.com>
2024-01-30 05:13:18 -0300

    chore: improvement next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 
 const nextConfig = {
   images: {
+    deviceSizes: [450, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     remotePatterns: [{
       protocol: 'http',
       hostname: 'localhost'
@@ -12,7 +13,8 @@ const nextConfig = {
     loader: 'default',
     path: '/_next/image'
   },
-  reactStrictMode: true
+  reactStrictMode: true,
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
