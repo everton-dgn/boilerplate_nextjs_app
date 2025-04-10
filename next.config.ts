4410aafaff0d764e9bf8a4e2afef76a6a78274a6
Éverton Toffanetto <evertondgn@hotmail.com>
2025-04-10 02:09:00 -0300

    fix(config): update Next.js and React versions, switch to turbopack for development and build scripts

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,25 @@
 import type { NextConfig } from 'next'
 
-const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
-
 const nextConfig: NextConfig = {
   // experimental: {
-    // ppr: 'incremental'
+  // ppr: 'incremental'
   // },
-  experimental: IS_DEVELOPMENT
-    ? {
-      turbo: {
-        rules: {
-          '*.svg': {
-            loaders: ['@svgr/webpack'],
-            as: '*.js',
-          },
-        },
-      },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js'
+      }
     }
-    : {},
-  webpack(config) {
-    if (!IS_DEVELOPMENT) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        use: ['@svgr/webpack'],
-      })
-    }
-    return config
   },
   images: {
     deviceSizes: [450, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    remotePatterns: [{
-      protocol: 'http',
-      hostname: 'localhost'
-    }],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost'
+      }
+    ],
     formats: ['image/webp'],
     loader: 'default',
     path: '/_next/image'
